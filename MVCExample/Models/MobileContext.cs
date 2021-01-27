41eb051a8d76dc6c702b304402a7bb944bd15381
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2021-01-27 23:00:14 +1000

    Добавлены модели Phone и Order и MobileContext для работы с БД

---
+++ MVCExample/Models/MobileContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace MVCExample.Models
+{
+    public class MobileContext : DbContext
+    {
+        public DbSet<Phone> Phones { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public MobileContext(DbContextOptions<MobileContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
--- MVCExample/Startup.cs
+++ MVCExample/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MVCExample.Models;
 
 namespace MVCExample
 {
@@ -23,29 +25,20 @@ namespace MVCExample
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<MobileContext>(options => options.UseSqlServer(connection));
+            
             services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            app.UseDeveloperExceptionPage();
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
