fe5b8bf632bf5f0da4c63e3e8be0471fa3667fef
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2021-01-28 00:51:17 +1000

    Добавил вывод списка мобильных телефонов

--- MVCExample/Controllers/HomeController.cs
+++ MVCExample/Controllers/HomeController.cs
@@ -11,27 +11,16 @@ namespace MVCExample.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private MobileContext db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(MobileContext context)
         {
-            _logger = logger;
+            db = context;
         }
 
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+            return View(db.Phones.ToList());
         }
     }
 }
