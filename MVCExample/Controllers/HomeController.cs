cc328a29d409cd2d7b12a626ec0498fa59b9595d
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2021-01-28 23:28:37 +1000

    Добавлено оформление покупки

--- MVCExample/Controllers/HomeController.cs
+++ MVCExample/Controllers/HomeController.cs
@@ -22,5 +22,25 @@ namespace MVCExample.Controllers
         {
             return View(db.Phones.ToList());
         }
+
+        [HttpGet]
+        public IActionResult Buy(int? id)
+        {
+            if (id == null)
+                RedirectToAction("Index");
+
+            ViewBag.PhoneId = id;
+                
+            return View();
+        }
+
+        [HttpPost]
+        public string Buy(Order order)
+        {
+            db.Orders.Add(order);
+            db.SaveChanges();
+            
+            return "Спасибо за покупку!";
+        }
     }
 }
