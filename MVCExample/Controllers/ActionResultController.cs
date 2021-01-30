86fb40d3993e4f71786ae81e914846e8971cf79f
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2021-01-30 23:29:12 +1000

    Добавил пример использования ActionResult

---
+++ MVCExample/Controllers/ActionResultController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCExample.Utils;
+
+namespace MVCExample.Controllers
+{
+    public class ActionResultController : Controller
+    {
+        
+        /// <summary>
+        /// Пример использования IActionResult
+        /// 
+        /// Пример:
+        /// /ActionResult/GetHtmlResult
+        /// </summary>
+        /// <returns></returns>
+        public HtmlResult GetHtmlResult()
+        {
+            return new HtmlResult("<h2>Привет ASP.NET 5</h2>");
+        }
+    }
+}
---
+++ MVCExample/Utils/HtmlResult.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCExample.Utils
+{
+    public class HtmlResult : IActionResult
+    {
+        private string _htmlCode;
+
+        public HtmlResult(string htmlCode)
+        {
+            _htmlCode = htmlCode;
+        }
+        
+        public async Task ExecuteResultAsync(ActionContext context)
+        {
+            string fullHtmlCode = "<!DOCTYPE html><html><head>";
+            fullHtmlCode += "<title>Главная страница</title>";
+            fullHtmlCode += "<meta charset=utf-8 />";
+            fullHtmlCode += "</head> <body>";
+            fullHtmlCode += _htmlCode;
+            fullHtmlCode += "</body></html>";
+            
+            await context.HttpContext.Response.WriteAsync(fullHtmlCode);
+        }
+    }
+}
