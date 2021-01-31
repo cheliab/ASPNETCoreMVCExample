df528dd209c1d1cf4581021eccaa01321924dc3a
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2021-01-31 19:45:14 +1000

    Добавлен пример с ContentResult и JsonResult

--- MVCExample/Controllers/ActionResultController.cs
+++ MVCExample/Controllers/ActionResultController.cs
@@ -17,5 +17,32 @@ namespace MVCExample.Controllers
         {
             return new HtmlResult("<h2>Привет ASP.NET 5</h2>");
         }
+
+        /// <summary>
+        ///
+        /// Пример:
+        /// /ActionResult/GetContentResult
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult GetContentResult()
+        {
+            return Content("Можно вывести какую то строку");
+        }
+
+        /// <summary>
+        ///
+        /// Пример:
+        /// /ActionResult/GetJsonResult
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult GetJsonResult()
+        {
+            // Пример со строкой
+            // return Json("Tom");
+            
+            // Пример с объектом
+            UserData userData = new UserData{ Name ="Paha", Age = 30 };
+            return Json(userData);
+        }
     }
 }
---
+++ MVCExample/Utils/UserData.cs
@@ -0,0 +1,8 @@
+﻿namespace MVCExample.Utils
+{
+    public class UserData
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
